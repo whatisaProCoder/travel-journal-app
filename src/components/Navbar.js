e0533cf0ea048280597d8675094822de81ba156c
whatisaProCoder <104326867+whatisaProCoder@users.noreply.github.com>
2022-04-25 21:12:47 +0530

    TEST

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <div className="navbar">
       <img src={require("../images/navbar-globe.png")} />
-      <p>my travel journal.</p>
+      <p>my travel journal. [TEST]</p>
     </div>
   );
 }
